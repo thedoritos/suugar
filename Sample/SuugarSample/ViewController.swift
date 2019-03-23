b8c456cecd510629daa18a476f275f8bc1d2fa1e
thedoritos <thedoritos@gmail.com>
2019-03-24 00:32:52 +0900

    Add sample app

---
+++ Sample/SuugarSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuugarSample
+//
+//  Created by thedoritos on 2019/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
