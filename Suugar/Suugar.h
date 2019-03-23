4ab464128dcbadf7769dbaabbe49c4dac35e5e52
thedoritos <thedoritos@gmail.com>
2019-03-23 23:55:44 +0900

    Create empty project

---
+++ Suugar/Suugar.h
@@ -0,0 +1,18 @@
+//
+//  Suugar.h
+//  Suugar
+//
+//  Created by thedoritos on 2019/03/23.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Suugar.
+FOUNDATION_EXPORT double SuugarVersionNumber;
+
+//! Project version string for Suugar.
+FOUNDATION_EXPORT const unsigned char SuugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Suugar/PublicHeader.h>
+
+
