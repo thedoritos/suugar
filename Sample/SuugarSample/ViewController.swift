6954aeb65f5344820c5e513756cd223e8cf05d1a
thedoritos <thedoritos@gmail.com>
2019-03-24 11:45:25 +0900

    Add first sample

--- Sample/SuugarSample/ViewController.swift
+++ Sample/SuugarSample/ViewController.swift
@@ -6,9 +6,18 @@
 //
 
 import UIKit
+import Suugar
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        ui {
+            $0.label {
+                $0.frame = CGRect(x: 0, y: 320, width: 375, height: 30)
+                $0.text = "Hello, World!"
+                $0.textAlignment = .center
+            }
+        }
     }
 }
